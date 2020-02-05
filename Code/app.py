716e7a88c1fa0c9b7cacf788daba7743cebd7aca
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-05 15:44:06 -0800

    [add] virtual env and app.py to make it work with Flask

---
+++ Code/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from histogram import Histogram, create_histogram, get_frequency, get_unique_words, word_contains
+from playground import sample_words, get_lines
+
+app = Flask(__name__)
+
+@app.route('/')
+def generate_words():
+    lines = get_lines()
+    myHistogram = Histogram(lines)
+    sentence = ""
+    num_words = 4
+    for i in range(num_words):
+        word = sample_words(myHistogram)
+        sentence += " " + word
+    return sentence
+
+if __name__ == '__main__':
+    app.run()
