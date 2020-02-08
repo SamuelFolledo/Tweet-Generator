526e46b41bd1fc9044358fda7e4950aaed2afe10
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-08 00:43:32 -0800

    [add] completed dictogram

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -15,7 +15,6 @@ class Dictogram(dict):
         self.tokens = 0  #total count of all words #tokens
         if word_list != None: #if list is not empty, update our properties
             for word in word_list:
-                print("words = ", word )
                 self.add_count(word)
                 # words_from_line = re.sub("[^\w]", " ",  line).split() #turns every word in line to a list of words
                 # for word in words: #loop through each word and get the histogram
@@ -57,22 +56,17 @@ class Dictogram(dict):
         sum_of_values = sum(self.values()) #word_counts.values() returns a list of word_count's values. sum() will sum a the values in a list and returns an int
         random_num = randint(0, sum_of_values - 1) #get a random num from 0-sum_of_values -1 
         random_weighted_word = ""
-        # for i in range(20): #test by doing it 10x
-            # print(f"Test #{i+1}: {random_num} =", end = " ") #end to not create a new line
         for w in self.items():
             if random_num == 0:
-                # print(f"{w[0]}\n")
                 random_weighted_word = w[0]
                 break
             if random_num > 0: #if rand_num is greater than 0, then decrement it
                 random_num -= w[1]
-            if random_num < 0: #0=one, 1-4=fish, 5=two, 6=red, 7 = blue
-                # print(f"{w[0]}\n")
+            if random_num < 0:
                 random_weighted_word = w[0]
                 break
         random_num = randint(0, sum_of_values - 1) # reset the random number
         return random_weighted_word
-
 
 def print_histogram(word_list):
     print()
@@ -95,34 +89,34 @@ def print_histogram_samples(histogram): #MARK: Find out why the word is always N
     samples_list = [histogram.sample() for _ in range(10000)]
     samples_hist = Dictogram(samples_list)
     print('samples: {}'.format(samples_hist))
-    # print()
-    # print('Sampled frequency and error from observed frequency:')
-    # header = '| word type | observed freq | sampled freq  |  error  |'
-    # divider = '-' * len(header)
-    # print(divider)
-    # print(header)
-    # print(divider)
-    # # Colors for error
-    # green = '\033[32m'
-    # yellow = '\033[33m'
-    # red = '\033[31m'
-    # reset = '\033[m'
-    # # Check each word in original histogram
-    # for word, count in histogram.items():
-    #     # Calculate word's observed frequency
-    #     observed_freq = count / histogram.tokens
-    #     # Calculate word's sampled frequency
-    #     samples = samples_hist.frequency(word)
-    #     sampled_freq = samples / samples_hist.tokens
-    #     # Calculate error between word's sampled and observed frequency
-    #     error = (sampled_freq - observed_freq) / observed_freq
-    #     color = green if abs(error) < 0.05 else yellow if abs(error) < 0.1 else red
-    #     print('| {!r:<9} '.format(word)
-    #         + '| {:>4} = {:>6.2%} '.format(count, observed_freq)
-    #         + '| {:>4} = {:>6.2%} '.format(samples, sampled_freq)
-    #         + '| {}{:>+7.2%}{} |'.format(color, error, reset))
-    # print(divider)
-    # print()
+    print()
+    print('Sampled frequency and error from observed frequency:')
+    header = '| word type | observed freq | sampled freq  |  error  |'
+    divider = '-' * len(header)
+    print(divider)
+    print(header)
+    print(divider)
+    # Colors for error
+    green = '\033[32m'
+    yellow = '\033[33m'
+    red = '\033[31m'
+    reset = '\033[m'
+    # Check each word in original histogram
+    for word, count in histogram.items():
+        # Calculate word's observed frequency
+        observed_freq = count / histogram.tokens
+        # Calculate word's sampled frequency
+        samples = samples_hist.frequency(word)
+        sampled_freq = samples / samples_hist.tokens
+        # Calculate error between word's sampled and observed frequency
+        error = (sampled_freq - observed_freq) / observed_freq
+        color = green if abs(error) < 0.05 else yellow if abs(error) < 0.1 else red
+        print('| {!r:<9} '.format(word)
+            + '| {:>4} = {:>6.2%} '.format(count, observed_freq)
+            + '| {:>4} = {:>6.2%} '.format(samples, sampled_freq)
+            + '| {}{:>+7.2%}{} |'.format(color, error, reset))
+    print(divider)
+    print()
 
 
 def main():
@@ -136,12 +130,12 @@ def main():
         word = 'abracadabra'
         print_histogram(list(word))
         # Test histogram on words in a classic book title
-        # fish_text = 'one fish two fish red fish blue fish'
-        # print_histogram(fish_text.split())
-        # # Test histogram on words in a long repetitive sentence
-        # woodchuck_text = ('how much wood would a wood chuck chuck'
-        #                   ' if a wood chuck could chuck wood')
-        # print_histogram(woodchuck_text.split())
+        fish_text = 'one fish two fish red fish blue fish'
+        print_histogram(fish_text.split())
+        # Test histogram on words in a long repetitive sentence
+        woodchuck_text = ('how much wood would a wood chuck chuck'
+                          ' if a wood chuck could chuck wood')
+        print_histogram(woodchuck_text.split())
 
 
 if __name__ == '__main__':
