a32bf3d9516365d655772b1841e2ebb470225bb0
Samuel Folledo <samuelfolledo@gmail.com>
2020-04-13 01:52:45 -0400

    [add] update now replaces data instead of deleting node and appending in the end

--- Code/linkedlist.py
+++ Code/linkedlist.py
@@ -145,7 +145,6 @@ class LinkedList(object):
                 print(f"Updating {item} to {data}")
                 # self.delete(item) #delete and append
                 # self.append(data)
-                #For future, create a update function instead of deleting and appending
                 item[1] = data[1]
 
 def test_linked_list():
